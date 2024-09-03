ad2bbe0ec6b92d99fc2dc9dc337986964cabcec7
Yujin Lee <dbwls9040@gmail.com>
2024-09-03 15:38:30 +0900

    프로필 등록 구현

--- src/main/java/com/sparta/springnewsfeed/domain/profile/repository/ProfileRepository.java
+++ src/main/java/com/sparta/springnewsfeed/domain/profile/repository/ProfileRepository.java
@@ -1,4 +1,9 @@
 package com.sparta.springnewsfeed.domain.profile.repository;
 
-public class ProfileRepository {
+import com.sparta.springnewsfeed.domain.profile.entity.Profile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProfileRepository extends JpaRepository<Profile, Long> {
 }
