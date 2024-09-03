81cfd810b46e15f264c4a390a645cf41572930ab
Yujin Lee <dbwls9040@gmail.com>
2024-09-03 15:38:30 +0900

    프로필 등록 구현

--- src/main/java/com/sparta/springnewsfeed/domain/profile/controller/ProfileController.java
+++ src/main/java/com/sparta/springnewsfeed/domain/profile/controller/ProfileController.java
@@ -1,4 +1,26 @@
 package com.sparta.springnewsfeed.domain.profile.controller;
 
+import com.sparta.springnewsfeed.domain.profile.dto.request.CreateProfileRequestDto;
+import com.sparta.springnewsfeed.domain.profile.dto.response.CreateProfileResponseDto;
+import com.sparta.springnewsfeed.domain.profile.service.ProfileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
 public class ProfileController {
+
+    private final ProfileService profileService;
+
+    @PostMapping("/profile/{id}")
+    public ResponseEntity<CreateProfileResponseDto> createProfile(@PathVariable(value = "id") Long id, @RequestBody CreateProfileRequestDto createProfileRequestDto){
+        return ResponseEntity.ok(profileService.createProfile(id, createProfileRequestDto));
+    }
+
+//    @GetMapping("/profile/{userId}")
+//    public ResponseEntity<GetProfileResponseDto> getProfile(@PathVariable(value = "userId") Long userId){
+//        return ResponseEntity.ok(profileService.getUserById(userId));
+//    }
 }
--- src/main/java/com/sparta/springnewsfeed/domain/profile/service/ProfileService.java
+++ src/main/java/com/sparta/springnewsfeed/domain/profile/service/ProfileService.java
@@ -1,4 +1,32 @@
 package com.sparta.springnewsfeed.domain.profile.service;
 
+import com.sparta.springnewsfeed.domain.profile.dto.request.CreateProfileRequestDto;
+import com.sparta.springnewsfeed.domain.profile.dto.response.CreateProfileResponseDto;
+import com.sparta.springnewsfeed.domain.profile.entity.Profile;
+import com.sparta.springnewsfeed.domain.profile.repository.ProfileRepository;
+import com.sparta.springnewsfeed.domain.user.entity.User;
+import com.sparta.springnewsfeed.domain.user.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class ProfileService {
+
+    private final ProfileRepository profileRepository;
+    private final UserRepository userRepository;
+
+    public CreateProfileResponseDto createProfile(Long id, CreateProfileRequestDto createProfileRequestDto) {
+        User user = userRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("선택한 유저가 존재하지 않습니다."));
+
+        // 1. 프로필 생성
+        Profile profile = new Profile(createProfileRequestDto, user);
+
+        // 2. db 저장
+        Profile saveProfile = profileRepository.save(profile);
+
+        // 3. 저장된 프로필과 게시글을 응답 DTO 생성 후 반환
+        return new CreateProfileResponseDto((saveProfile));
+    }
 }
