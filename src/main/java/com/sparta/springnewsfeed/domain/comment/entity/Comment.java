ddd6fb9cb422945824b28255818c78cb9ca662c5
pookyspooky <union0802@naver.com>
2024-09-03 19:40:25 +0900

    id => comment_id로 수정

--- src/main/java/com/sparta/springnewsfeed/domain/comment/entity/Comment.java
+++ src/main/java/com/sparta/springnewsfeed/domain/comment/entity/Comment.java
@@ -15,6 +15,7 @@ public class Comment extends Timestamped {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
     private Long id;
 
     @Column(nullable = false, length = 500)
