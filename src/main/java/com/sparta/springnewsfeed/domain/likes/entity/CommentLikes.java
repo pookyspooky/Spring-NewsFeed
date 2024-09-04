6773b90abad5684849b5668e4821f9ab61630da4
pookyspooky <union0802@naver.com>
2024-09-05 01:41:33 +0900

    Likes 패키지 FetchType.LAZY 추가

--- src/main/java/com/sparta/springnewsfeed/domain/likes/entity/CommentLikes.java
+++ src/main/java/com/sparta/springnewsfeed/domain/likes/entity/CommentLikes.java
@@ -19,11 +19,11 @@ public class CommentLikes extends Timestamped {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "comment_id")
     private Comment comment;
 
